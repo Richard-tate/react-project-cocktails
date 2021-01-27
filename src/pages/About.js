00733413487e971f36d8c3660c2e38de00e046ce
Richard Tate <rjtate85@gmail.com>
2021-01-27 12:10:06 +0000

    init

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+  
+        <h1 className="section-title">about us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Porro ut, deleniti sint mollitia adipisci, ipsum voluptatem pariatur blanditiis facere esse ipsam similique magni atque facilis. 
+          Debitis corrupti obcaecati incidunt voluptate.
+        </p>
+      
+    </section>
+  )
+}
+
+export default About;
